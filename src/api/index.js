318c2150bcb0a997732234db23c85e515bce25d5
Pradeep Joshi <pradeepjoshi@KELLGGNLPTP1190.local>
2023-05-07 23:02:53 +0530

    one param change

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getTopHeadlineRequest = async params => {
-  const endpoint = `https://api.themoviedb.org/3/movie/${'top_rated'}?api_key=a531b8aedefc4f260e0a74e88e6f86a0&language=en-US&page=1`;
+  const endpoint = `https://api.themoviedb.org/3/movie/${params.endPoint}?api_key=a531b8aedefc4f260e0a74e88e6f86a0&language=en-US&page=1`;
   var res = null;
   res = await axios.get(endpoint);
   return res.data.results;
